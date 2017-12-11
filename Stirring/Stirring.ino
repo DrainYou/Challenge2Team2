a606b332289e6c6cbd9e9ccbb462ac172fee3c0a
YankeZhang <32335582+YankeZhang@users.noreply.github.com>
2017-12-11 10:40:52 +0000

    Update Stirring.ino

--- Stirring/Stirring.ino
+++ Stirring/Stirring.ino
@@ -4,8 +4,55 @@
 int motorspeed;
 volatile int rpm;
 volatile int period;
-int analogspeed;
+float analogspeed;
 long lastMillis=0;
+
+void setup()
+{ 
+  Serial.begin(9600);
+  pinMode(readsensor,INPUT);
+  pinMode(power,OUTPUT);
+  attachInterrupt(readsensor,measure,RISING);
+  analogspeed=0.0;
+  motorspeed=1500;
+  rpm=0;
+  interrupts();
+}
+
+void loop()
+{
+  Serial.println(rpm);
+
+  if(Serial.available()>0)
+    {
+      motorspeed=Serial.read();
+      
+    }
+  if(motorspeed<=1500&&motorspeed>=500)
+    { 
+      if(analogspeed>127)
+        {
+          analogspeed--;
+        }
+      else if(rpm<motorspeed)
+          {
+             analogspeed+=0.08;
+             analogWrite(power,analogspeed/2);
+          }
+      else if(rpm==motorspeed)
+          {
+            analogWrite(power,analogspeed/2);
+          }
+      else
+          {
+            analogspeed-=0.08; 
+            analogWrite(power,analogspeed/2);
+          }
+    }
+    else
+      analogWrite(power,0);
+    
+}
 
 void measure()
 {
@@ -13,59 +60,3 @@ void measure()
   lastMillis=millis();
   rpm=60000/period;
 }
-
-void setup()
-{ 
-  Serial.begin(9600);
-  pinMode(readsensor,INPUT);
-  pinMode(power,OUTPUT);
-  
-//  pinMode(switch1,INPUT_PULLUP);
-//  pinMode(switch2,INPUT_PULLUP);
-  
-  attachInterrupt(readsensor,measure,RISING);
-  analogspeed=20;
-  do {
-  } while (Serial.available()==0);
-
-  motorspeed = Serial.read();
-  motorspeed = motorspeed * 20;
-  
-  rpm=0;
-  interrupts();
-}
-
-
-void loop()
-{
-  Serial.println(rpm);
-  
-  if(motorspeed<=1500&&motorspeed>=500)
-    { 
-      while(analogspeed>127)
-        {
-          analogspeed=127;
-        }
-        
-      if(rpm<motorspeed)
-          {
-             analogspeed++;
-             analogWrite(power,analogspeed);
-          }
-      else if(rpm==motorspeed)
-          {
-            analogWrite(power,analogspeed);
-          }
-      else
-          {
-            analogspeed--; 
-            analogWrite(power,analogspeed);
-          }
-    }else{
-      analogspeed = 0;
-      analogWrite(power,analogspeed);
-    }
-    delay(100);
-}
-
-
